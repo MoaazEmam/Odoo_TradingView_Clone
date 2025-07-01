0d5fa6a51b620d9c7df12406bc6c7b35b5a52e05
Moaaz Emam <moaaz.emam06@eng-st.cu.edu.eg>
2025-07-01 03:58:40 +0300

    Inital commit, added tradingview_clone module and symbol model

---
+++ custom-addons/tradingview_clone/models/tradingview_symbol.py
@@ -0,0 +1,22 @@
+from odoo import models,fields
+
+class TradingViewSymbol(models.Model):
+    _name='tradingview.symbol'
+    _description='Trading Symbol'
+    name = fields.Char() # Full company/asset name
+    symbol = fields.Char() # Trading symbol (e.g., BTCUSD, AAPL)
+    slug = fields.Char() # URL-safe slug (e.g., btcusd)
+    exchange = fields.Char() # Exchange name or code
+    region = fields.Char() # Country or market region
+    currency = fields.Char() # Trading currency (USD, EUR, BTC,etc.)
+    sector = fields.Char() # Sector or asset category
+    industry = fields.Char() # Industry classification (if applicable)
+    isin = fields.Char() # International Securities Identification Number (optional)
+    active = fields.Boolean(default=True)
+    type = fields.Selection([
+    ('stock', 'Stock'),
+    ('crypto', 'Cryptocurrency'),
+    ('forex', 'Forex'),
+    ('commodity', 'Commodity'),
+    ('index', 'Index'),
+], default='stock')
